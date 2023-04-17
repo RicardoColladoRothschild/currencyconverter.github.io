d2d47127ebe2da47be13d037e81632a79a5c05b3
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-16 21:49:16 -0400

    add a select with all of the possible symbols

--- scripts.js
+++ scripts.js
@@ -4,6 +4,11 @@ const API_SYMBOLS = "https://api.apilayer.com/fixer/symbols";
 var myHeaders = new Headers();
 myHeaders.append("apikey", "lRvWzjhj88KvkMW2L2JvWWncagN6os2C");
 
+//HTML tags references:
+let CURRENCY_SYMBOLS_CONTAINER = document.getElementById('currencySymbols-container');
+let CURRENCY_OPTIONS = document.getElementById('currencySymbols');
+
+
 var requestOptions = {
   method: 'GET',
   redirect: 'follow',
@@ -41,8 +46,14 @@ fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
         let symbolsObjectKeys = Object.keys(symbolsArray[1]);
         let symbolsObjectVlue = Object.values(symbolsArray[1]);
 
-          for(let index = 0; index < symbolsObjectKeys.length;index++){
+          
+          
 
-              console.log(`Object key: ${symbolsObjectKeys[index]} | object value: ${symbolsObjectVlue[index]}`);
-          }
+            symbolsObjectKeys.forEach((symbol)=>{
+                  
+                  let symbolOption = document.createElement('option');
+                  symbolOption.setAttribute('value',symbol)
+                  symbolOption.innerHTML = symbol;
+                  CURRENCY_OPTIONS.append(symbolOption);
+            });
     }
